54c1a99278141fd69b6da661d55f6da4019a44b5
Collins <jbcollins254@gmail.com>
2024-05-04 18:16:43 +0300

    sixeth commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Routes,Route } from 'react-router-dom';
 
 function App() {
   const [bots, setBots] = useState([]);
+  const [army, setArmy] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:3000/bots")
@@ -14,7 +15,14 @@ function App() {
       .then(data => setBots(data))
       .catch(error => console.error('Error fetching bots:', error));
   }, []);
+  const handleAddToArmy = (bot) => {
+    console.log("added")
+    setArmy(prevArmy => [...prevArmy, bot]);
+  };
 
+  const handleReleaseFromArmy = (bot) => {
+    setArmy(prevArmy => prevArmy.filter(item => item.id !== bot.id));
+  };
   return (
     <div className="App">
       <h1>Bot Army</h1>
--- src/components/BotDetails.jsx
+++ src/components/BotDetails.jsx
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-function BotDetails({bots}) {
+function BotDetails({bots }) {
   const { id } = useParams();
   const [bot, setBot] = useState([]);
- 
+  const [army, setArmy] = useState([])
 
   useEffect(() => {
   
@@ -18,21 +18,25 @@ function BotDetails({bots}) {
       })
    
   }, []); 
-  console.log(bot)
-
  
 
+  const handleAddToArmy = (bot) => {
+    console.log("added")
+    setArmy(prevArmy => [...prevArmy, bot]);
+  };
+console.log(army)
   return (
     <div className="bot-details">
       <img src={bot.avatar_url} alt={bot.name} />
       <h3>{bot.name}</h3>
       <p>Class: {bot.bot_class}</p>
+      <p>{bot.catchphrase}</p>
       <p>❤️: {bot.health}</p>
       <p>💀: {bot.damage}</p>
      <p>Armor: {bot.armor}</p>
      <button> <Link to={"/"}>Back</Link></button>
     
-      <button>Add To Army</button>
+      <button onClick={handleAddToArmy}>Add To Army</button>
     </div>
   );
 }
